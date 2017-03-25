44b5dda7327453df675741cedd262c52e8354059
August Burns <shturm@urghh.net>
2017-03-25 18:13:27 +0200

    add files

---
+++ content.js
@@ -0,0 +1,9 @@
+// Checking page title
+if (document.title.indexOf("Strategy") != -1) {
+    //Creating Elements
+    var btn = document.createElement("BUTTON")
+    var t = document.createTextNode("CLICK ME");
+    btn.appendChild(t);
+    //Appending to DOM 
+    document.body.appendChild(btn);
+}
